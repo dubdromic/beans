04846072dee6c365983f184a01dbdcf26325ca20
Corin Schedler <corin@dubdromic.com>
2014-11-24 19:12:51 -0600

    Update to rspec 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'beans'
 
 RSpec.configure do |config|
   config.raise_errors_for_deprecations!
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
